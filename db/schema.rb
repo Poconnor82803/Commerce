9aedb0f4ca783d159891c41688be72e3dd4fe3d4
“PATRICK <PATRICK.T.OCONNOR@MAINE.EDUssh-keygen -t rsa -C PATRICK.T.OCONNOR@MAINE.EDU>
2016-03-13 14:27:18 -0400

    added books db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160313162946) do
+ActiveRecord::Schema.define(version: 20160313172909) do
+
+  create_table "books", force: :cascade do |t|
+    t.string   "title"
+    t.string   "author"
+    t.string   "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
